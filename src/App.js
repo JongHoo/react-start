6194da111ff9f05252d27f9cc80dc58cdeec626c
JongHoo <monkeis7@gmail.com>
2019-03-29 22:53:51 +0900

    developing...

--- src/App.js
+++ src/App.js
@@ -1,11 +1,62 @@
 import React, { Component } from 'react'
 import TodoListTemplate from './components/TodoListTemplate'
+import Form from './components/Form'
+import TodoItemList from "./components/TodoItemList";
 
 class App extends Component {
+
+  id = 3
+  state = {
+    input: '',
+    todos: [
+      { id: 0, text: 'test1', checked: false},
+      { id: 1, text: 'test2', checked: true},
+      { id: 2, text: 'test3', checked: false},
+    ]
+  }
+
+  handleChange = (e) => {
+    this.setState({
+      input: e.target.value
+    })
+  }
+
+  handleCreate = () => {
+    const { input, todos } = this.state
+    this.setState({
+      input: '',
+      todos: todos.concat({
+        id: this.id++,
+        text: input,
+        checked: false
+      })
+    })
+  }
+
+  handleKeyPress = (e) => {
+    if(e.key === 'Enter') {
+      this.handleCreate()
+    }
+  }
+
   render() {
+    const { input } = this.state
+    const {
+      handleChange,
+      handleCreate,
+      handleKeyPress
+    } = this
+
     return (
-      <TodoListTemplate>
-        Test
+      <TodoListTemplate form={(
+        <Form
+          value={input}
+          onKeyPress={handleKeyPress}
+          onChange={handleChange}
+          onCreate={handleCreate}
+        />
+      )}>
+        <TodoItemList/>
       </TodoListTemplate>
     )
   }
--- src/components/TodoListTemplate.js
+++ src/components/TodoListTemplate.js
@@ -2,19 +2,19 @@ import React from 'react'
 import './TodoListTemplate.css'
 
 const TodoListTemplate = ({form, children}) => {
-    return (
-        <main className="todo-list-template">
-            <div className="title">
-                오늘 할 일
-            </div>
-            <section className="form-wrapper">
-                {form}
-            </section>
-            <sectionn className="todos-wrapper">
-                {children}
-            </sectionn>
-        </main>
-    )
+  return (
+    <main className="todo-list-template">
+      <div className="title">
+        오늘 할 일
+      </div>
+      <section className="form-wrapper">
+        {form}
+      </section>
+      <sectionn className="todos-wrapper">
+        {children}
+      </sectionn>
+    </main>
+  )
 }
 
 export default TodoListTemplate
